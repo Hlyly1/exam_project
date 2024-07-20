24a1092a0eda3d02d1c173dedabc325485399830
ly <2547364862@qq.com>
2024-07-20 19:37:45 +0800

    fetch 权限路由

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
-type RouterDataType = typeof routersData
-export type RouterKeys = keyof RouterDataType
-
+type RouterDataType = typeof routersData;
+export type RouterKeys = keyof RouterDataType;
 
 export const routersData = {
   login: {
@@ -64,3 +63,57 @@ export const routersData = {
     hasMenu: true,
   },
 };
+
+export const studentMenus = [
+  {
+    label: "考题管理",
+    key: "exam_select",
+  },
+  {
+    label: "考试记录",
+    key: "exam_history",
+  },
+];
+
+
+export const adminMenus = [
+  {
+    label: "阅卷列表",
+    key: "corret_exam_list",
+  },
+  {
+    label: "考试管理",
+    key: "subject_add",
+  },
+  {
+    label: "课程管理",
+    key: "subject_manage",
+  },
+  {
+    label: "学员管理",
+    key: "student_manage",
+  },
+];
+
+export const superAdminMenus = [
+  {
+    label: "阅卷列表",
+    key: "corret_exam_list",
+  },
+  {
+    label: "考题管理",
+    key: "subject_add",
+  },
+  {
+    label: "课程管理",
+    key: "subject_manage",
+  },
+  {
+    label: "学员管理",
+    key: "student_manage",
+  },
+  {
+    label: "管理员管理",
+    key: "admin_manage",
+  }
+];
