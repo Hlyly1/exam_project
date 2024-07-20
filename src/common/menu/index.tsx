ff8a99fe5dbb453521cf56efb106ae1b163931df
ly <2547364862@qq.com>
2024-07-20 19:58:03 +0800

    fetch:菜单显示

--- src/common/menu/index.tsx
+++ src/common/menu/index.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
-import { superAdminMenus } from '@/config';
+import { RouterKeys, routersData, superAdminMenus } from '@/config';
+import { useNavigate } from 'react-router-dom';
+import usePathKey from '@/hooks/usePathKey';
 
 
 
 const App: React.FC = () => {
   const [current, setCurrent] = useState('');
-  
+  const Navigate = useNavigate()
+  const path_key = usePathKey()
   const menes = superAdminMenus
+  useEffect(()=>{
+    if(path_key){
+      setCurrent(path_key)
+    }
+  },[])
   const onClick: MenuProps['onClick'] = (e) => {
     setCurrent(e.key);
+    Navigate(routersData[e.key as RouterKeys].path)
   };
 
   return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={menes} />;
--- src/hooks/usePathKey.tsx
+++ src/hooks/usePathKey.tsx
@@ -2,11 +2,11 @@ import { useLocation } from "react-router-dom"
 
 import { type RouterKeys } from "@/config";
 
-function useIsShowMenu(){
+function usePathKey(){
     const location = useLocation()
     const key:RouterKeys = location.pathname.split('/')[1] as RouterKeys
     
     return key
 }
 
-export default useIsShowMenu
+export default usePathKey
