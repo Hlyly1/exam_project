a8508c6bac3e2f0ecb1e57d20e39b34eb3a74ab3
ly <2547364862@qq.com>
2024-07-26 22:57:42 +0800

    subject_add_list

--- src/page/subject_add/index.tsx
+++ src/page/subject_add/index.tsx
@@ -1,59 +1,12 @@
-import { get_subject_tree_async } from "@/store/slice/subject";
+import { get_subject_tree_async, select_subject_tree } from "@/store/slice/subject";
 import style from "./index.module.css";
 import { TreeSelect } from "antd";
 import { TreeSelectProps, Button } from "antd";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "@/store";
 function SubjectAdd() {
-  const treeData = [
-    {
-      value: "parent 1",
-      title: "parent 1",
-      children: [
-        {
-          value: "parent 1-0",
-          title: "parent 1-0",
-          children: [
-            {
-              value: "leaf1",
-              title: "leaf1",
-            },
-            {
-              value: "leaf2",
-              title: "leaf2",
-            },
-            {
-              value: "leaf3",
-              title: "leaf3",
-            },
-            {
-              value: "leaf4",
-              title: "leaf4",
-            },
-            {
-              value: "leaf5",
-              title: "leaf5",
-            },
-            {
-              value: "leaf6",
-              title: "leaf6",
-            },
-          ],
-        },
-        {
-          value: "parent 1-1",
-          title: "parent 1-1",
-          children: [
-            {
-              value: "leaf11",
-              title: <b style={{ color: "#08c" }}>leaf11</b>,
-            },
-          ],
-        },
-      ],
-    },
-  ];
+  const treeData = useSelector(select_subject_tree)
   const [value, setValue] = useState<string>();
   const dispatch: AppDispatch = useDispatch();
   useEffect(() => {
--- src/store/slice/subject.ts
+++ src/store/slice/subject.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "@/util/http";
 import { AxiosRes } from "../../util/http";
-// import { RootState } from "../index";
+import { RootState } from "../index";
 
 const initialState = {
   subject_tree: [], //课程结构
@@ -11,7 +11,9 @@ const initialState = {
 export const get_subject_tree_async = createAsyncThunk(
   "/get/subject_tree",
   async (action, state) => {
-    const res: AxiosRes = await axios.get("/api/subject");
+    const res: AxiosRes = await axios.get("/api/subject/list");
+    console.log(res,'res');
+    
     return res.data.data;
   }
 );
@@ -20,9 +22,16 @@ export const sujectSlice = createSlice({
   name: "subject",
   initialState,
   reducers: {},
-  //   extraReducers: (builder) => {
-  //     builder;
-  //   },
+    extraReducers: (builder) => {
+      builder.addCase(get_subject_tree_async.fulfilled,(state,res)=>{
+        state.subject_tree = res.payload
+        console.log(state.subject_tree,'subject_tree');
+        
+      })
+    },
 });
 
+export const select_subject_tree = (state:RootState)=>{
+  return state.subject.subject_tree
+}
 export default sujectSlice.reducer;
